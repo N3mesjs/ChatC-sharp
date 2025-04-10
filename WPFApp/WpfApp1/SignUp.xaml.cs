df2c5954a5eab7948194c55503725f12aa6751b4
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2025-04-10 10:55:51 +0200

    Creation TCP Server
    TCP Server project creation and some edits to the signUp page

---
+++ WPFApp/WpfApp1/SignUp.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for SignUp.xaml
+    /// </summary>
+    public partial class SignUp : Window
+    {
+        private string _username;
+        private string _password;
+
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void sendForm_Click(object sender, RoutedEventArgs e)
+        {
+            _username = userName.Text;
+            _password = passWord.Password;
+        }
+    }
+}
